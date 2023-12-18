1e91ddd91b0352f0f076c31e2f12b22a17a2239b
bsong1124 <bsong1124@gmail.com>
2023-12-18 12:30:03 -0500

    add Home and MyTrips pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1>Home component</h1>
+    )
+}
+
+export default Home
---
+++ src/pages/MyTrips.jsx
@@ -0,0 +1,7 @@
+const MyTrips = () => {
+    return (
+        <h1>MyTrips component</h1>
+    )
+}
+
+export default MyTrips
