9f92a609f7a8e79d0add9f7091fe7a772c8a36ba
Zach Kurfirst <zachmk11@gmail.com>
2023-12-19 11:56:53 -0500

    add login functionality

---
+++ src/components/Auth/LoginButton.jsx
@@ -0,0 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/",
+      },
+    });
+  };
+
+  return <button onClick={handleLogin}>Log In</button>;
+};
+
+export default LoginButton;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,21 @@
-import {Link} from "react-router-dom"
-import "./Nav.css"
+import { Link } from "react-router-dom";
+import "./Nav.css";
+import LoginButton from "../Auth/LoginButton";
 
 const Header = () => {
-    return (
-        <nav className="Nav">
-            <Link to="/">Home</Link>
-            <Link to="/trips">My Trips</Link>
-            {/* <Link to="/trips/:id/activities">Activities</Link> */}
-            <Link to="/about">About</Link>
-        </nav>
-    )
-}
+  return (
+    <nav className="Nav">
+      <Link to="/">Home
+        {/* <img src={brandImage} /> */}
+      </Link>
+      <div>
+        <LoginButton />
+      </div>
+      <Link to="/trips">My Trips</Link>
+      {/* <Link to="/trips/:id/activities">Activities</Link> */}
+      <Link to="/about">About</Link>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
