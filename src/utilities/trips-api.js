36ac9578cc47668984f96fabd983b9d8b063e0d1
bsong1124 <bsong1124@gmail.com>
2023-12-19 12:54:29 -0500

    start delete functionality

--- src/utilities/trips-api.js
+++ src/utilities/trips-api.js
@@ -26,11 +26,24 @@ export async function index() {
   }
 }
 
-export async function show (id) {
-    const res = await fetch(config.BASE_URL + `/${id}`, {method: "GET"})
-    if(res.ok) {
-        return res.json()
-    } else {
-        console.log(err.message)
-    }
+export async function show(id) {
+  const res = await fetch(config.BASE_URL + `/${id}`, { method: "GET" });
+  if (res.ok) {
+    return res.json();
+  } else {
+    console.log(err.message);
+  }
+}
+
+export async function destroy(id) {
+  let endpoint = `${config.BASE_URL}/${id}`;
+  const response = await fetch(endpoint, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Invalid Request");
+  }
 }
