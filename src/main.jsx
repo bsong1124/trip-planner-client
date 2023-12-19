0a79575b96e005cb9586550421826aca0af3ff73
Zach Kurfirst <zachmk11@gmail.com>
2023-12-19 11:21:16 -0500

    save main.jsx with auth0 provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 
 import {BrowserRouter as Router} from 'react-router-dom'
+import Auth0ProviderWithNavigate from './components/Auth/Auth0ProviderWithNavigate.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
+    <Auth0ProviderWithNavigate>
     <App />
+    </Auth0ProviderWithNavigate>
   </Router>,
 )
