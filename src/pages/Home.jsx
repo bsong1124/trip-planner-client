449ba05d0779c838dbb6335ed9d16bbb5b52745f
Zach Kurfirst <zachmk11@gmail.com>
2023-12-20 17:47:09 -0500

    home page conditional rendering of CTA, button style in nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 import LoginButton from "../components/Auth/LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div className="home-wrapper">
-                <h1 className='header-title'>Welcome to Journey Craft</h1>
-                <LoginButton />
-        </div>
-    );
-}
+  const { user, isLoading: loadingAuth, isAuthenticated } = useAuth0();
+
+  return (
+    <div className="home-wrapper">
+      <h1 className="header-title">Welcome to Journey Craft</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      {isAuthenticated && !loadingAuth ? (
+        <Link to="/trips" className="btn-primary">
+          My Trips
+        </Link>
+      ) : (
+        <LoginButton />
+      )}
+    </div>
+  );
+};
 
 export default Home;
